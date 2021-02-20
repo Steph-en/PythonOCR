00859ad492de8a2e5092e15b55cf13078c3d9555
Steph-en <appahstephen9@gmail.com>
2021-02-20 16:07:19 +0000

    modified OCR-test

--- OCR-test.py
+++ OCR-test.py
@@ -2,7 +2,10 @@ import cv2
 import numpy as mp
 import pytesseract  
 
-pytesseract.pytesseract.tsseract_cmd = r"file:///home/snow/Downloads/tesseract-master"
+pytesseract.pytesseract.tsseract_cmd = r"x-special/nautilus-clipboard
+copy
+file:///home/snow/Downloads/tesseract-master/autogen.sh
+"
 
 img = cv2.imread(5832296680521840942.jpg)
 
